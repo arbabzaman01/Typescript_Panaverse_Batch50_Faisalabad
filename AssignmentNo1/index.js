bca252045764668ff7fb04af73853510f46c77f7
arbabzaman01 <gilaniarbab03@gmail.com>
2023-08-16 18:14:44 +0500

    first commit

---
+++ AssignmentNo1/index.js
@@ -0,0 +1,73 @@
+"use strict";
+//Print a message like " Hey Abu Hurairah, i have started  learning TYPESCRIPT "
+console.log("Hey Abu Hurairah, i have started  learning TYPESCRIPT");
+//Store your name in  a variable and print it
+var Name = "Arbab Zaman";
+console.log("your name is:");
+console.log(Name);
+//Store 10 numbers in different variables
+var Num1 = 1;
+console.log("1st number is :");
+console.log(Num1);
+var Num2 = 2;
+console.log("2nd number is:");
+console.log(Num2);
+var Num3 = 3;
+console.log("3rd number is:");
+console.log(Num3);
+var Num4 = 4;
+console.log("4th number is:");
+console.log(Num4);
+var Num5 = 5;
+console.log("5th number is :");
+console.log(Num5);
+var Num6 = 6;
+console.log("6th number is:");
+console.log(Num6);
+var Num7 = 7;
+console.log("7th number is:");
+console.log(Num7);
+var Num8 = 8;
+console.log("8th number is :");
+console.log(Num8);
+var Num9 = 9;
+console.log("9th number is:");
+console.log(Num9);
+var Num10 = 10;
+console.log("10th number is:");
+console.log(Num10);
+//Add all of them and print the sum
+var ProductOfAll = Num1 + Num2 + Num3 + Num4 + Num5 + Num6 + Num7 + Num8 + Num9 + Num10;
+console.log("Sum of all Ten numbers:");
+console.log(ProductOfAll);
+//Print the difference (substraction)
+var DifferenceOfAll = Num1 - Num2 - Num3 - Num4 - Num5 - Num6 - Num7 - Num8 - Num9 - Num10;
+console.log("Substraction of all Ten numbers:");
+console.log(DifferenceOfAll);
+//Print the result after multiplying all
+var MultiplicationOfAll = Num1 * Num2 * Num3 * Num4 * Num5 * Num6 * Num7 * Num8 * Num9 * Num10;
+console.log("Multiplication of all Ten numbers:");
+console.log(MultiplicationOfAll);
+//Take two numbers and print the division result
+var num1 = 3;
+var num2 = 7;
+var DivisonOfNum = num1 / num2;
+console.log("Divison of twon numbers: ");
+console.log(DivisonOfNum);
+//Now perform all four operations with the given numbers,print  the results,and observe the order in which  the operations take place
+//Addition (+)
+var SumOfNum = num1 + num2;
+console.log("Sum of Number: ");
+console.log(SumOfNum);
+//Substraction (-)
+var SubstractionOfNum = num1 - num2;
+console.log("Substraction of Number:");
+console.log(SubstractionOfNum);
+//Multiplication(*)
+var MultiplicationOfNum = num1 * num2;
+console.log("Multiplication of Number:");
+console.log(MultiplicationOfNum);
+//Divison (/)
+var DivisonOfNum = num1 / num2;
+console.log("Divison of Number:");
+console.log(DivisonOfNum);
---
+++ AssignmentNo1/index.ts
@@ -0,0 +1,72 @@
+//Print a message like " Hey Abu Hurairah, i have started  learning TYPESCRIPT "
+console.log("Hey Abu Hurairah, i have started  learning TYPESCRIPT");
+//Store your name in  a variable and print it
+var Name:string="Arbab Zaman";
+console.log("your name is:");
+console.log(Name);
+//Store 10 numbers in different variables
+var Num1:number=1;
+console.log("1st number is :");
+console.log(Num1);
+var Num2:number=2;
+console.log("2nd number is:");
+console.log(Num2);
+var Num3:number=3;
+console.log("3rd number is:");
+console.log(Num3);
+var Num4:number=4;
+console.log("4th number is:");
+console.log(Num4);
+var Num5:number=5;
+console.log("5th number is :");
+console.log(Num5);
+var Num6:number=6;
+console.log("6th number is:");
+console.log(Num6);
+var Num7:number=7;
+console.log("7th number is:");
+console.log(Num7);
+var Num8:number=8;
+console.log("8th number is :");
+console.log(Num8);
+var Num9:number =9;
+console.log("9th number is:");
+console.log(Num9);
+var Num10:number =10;
+console.log("10th number is:");
+console.log(Num10);
+//Add all of them and print the sum
+var ProductOfAll:number=Num1+Num2+Num3+Num4+Num5+Num6+Num7+Num8+Num9+Num10;
+console.log("Sum of all Ten numbers:");
+console.log(ProductOfAll);
+//Print the difference (substraction)
+var DifferenceOfAll:number=Num1-Num2-Num3-Num4-Num5-Num6-Num7-Num8-Num9-Num10;
+console.log("Substraction of all Ten numbers:");
+console.log(DifferenceOfAll);
+//Print the result after multiplying all
+var MultiplicationOfAll:number=Num1*Num2*Num3*Num4*Num5*Num6*Num7*Num8*Num9*Num10;
+console.log("Multiplication of all Ten numbers:");
+console.log(MultiplicationOfAll);
+//Take two numbers and print the division result
+var num1:number=3;
+var num2:number=7;
+var DivisonOfNum:number=num1/num2;
+console.log("Divison of twon numbers: ");
+console.log(DivisonOfNum);
+//Now perform all four operations with the given numbers,print  the results,and observe the order in which  the operations take place
+//Addition (+)
+var SumOfNum:number=num1+num2;
+console.log("Sum of Number: ");
+console.log(SumOfNum);
+//Substraction (-)
+var SubstractionOfNum:number=num1-num2;
+console.log("Substraction of Number:");
+console.log(SubstractionOfNum);
+//Multiplication(*)
+var MultiplicationOfNum:number=num1*num2;
+console.log("Multiplication of Number:");
+console.log(MultiplicationOfNum);
+//Divison (/)
+var DivisonOfNum:number=num1/num2;
+console.log("Divison of Number:");
+console.log(DivisonOfNum);
